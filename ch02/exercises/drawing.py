99069cbcef9a3314dc562f1af4eb937e817e1f6b
Kenny Yeung <kyeung26@binghamton.edu>
2023-02-11 00:59:20 -0500

    2/11 drawing

--- ch02/exercises/drawing.py
+++ ch02/exercises/drawing.py
@@ -1,1 +1,11 @@
 import turtle
+sides = int(input("Enter the number of sides"))
+length = int(input("Enter the length of each side"))
+my_turtle = turtle.Turtle()
+my_turtle.color('red')
+window = turtle.Screen()
+for i in range(sides):
+    my_turtle.forward(length)
+    my_turtle.left(360/sides)
+print("Done")  
+window.exitonclick()
