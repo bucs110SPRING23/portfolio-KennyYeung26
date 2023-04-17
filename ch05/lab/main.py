bc1aff9a28dab4cf836311a3de8fb9feafbfe588
Kenny Yeung <kyeung26@binghamton.edu>
2023-04-17 13:55:36 -0400

    lab05 corrections

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -22,20 +22,21 @@ def graph_coordinates(threenplus1_iters_dict):
     pygame.init()
     while 1:
         pygame.event.pump()
-        display = pygame.display.set_mode()
-        pygame.draw.lines(display, "blue", False, list(threenplus1_iters_dict.items()))
+        display = pygame.display.set_mode(size=(640, 480))
+        pygame.draw.lines(display, "blue", False, list(threenplus1_iters_dict.items()), width=1)
         dictionary = list(threenplus1_iters_dict.items())
         max_so_far = 0
         for i in range(len(dictionary)):
             if (dictionary[i][1] > max_so_far):
                 max_so_far = dictionary[i][1]
-        new_display = pygame.transform.flip(display, False, False)
-        width, height = new_display.get_size()
-        new_display = pygame.transform.scale(new_display, (width * 5, height * 5))
+        width, height = display.get_size()
+        new_display = pygame.transform.scale(display, (width * 5, height * 5))
+        display.blit(new_display, (0, 0))
+        new_display = pygame.transform.flip(display, False, True)
         display.blit(new_display, (0, 0))
         font = pygame.font.Font(None, 48)
         msg = font.render(("Max so far is: "+ str(max_so_far)), False, "Blue")
-        display.blit(msg, (1150,10))
+        display.blit(msg, (300,10))
         pygame.display.flip()
         break
     pygame.time.wait(6000)
